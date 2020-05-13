ed24f0de632b694c4c63dba02000c528aa68de94
Giovanni Luro <giovanni.luro@outlook.com>
2020-05-13 19:07:58 -0300

    sending password reset email

--- src/modules/users/controllers/ForgotPasswordController.ts
+++ src/modules/users/controllers/ForgotPasswordController.ts
@@ -1,20 +1,25 @@
 import { Request, Response } from 'express';
 import UsersRepository from '@modules/users/infra/typeorm/repositores/UsersRepository';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositores/UserTokensRepository';
-import MailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+import MailProvider from '@shared/container/providers/MailProvider/implementations/EtherealMailProvider';
 import SendEmailService from '@modules/users/services/SendForgotPasswordEmailService';
 
 export default class ForgotPasswordController {
+
+  private mailProvider: MailProvider;
+
+  constructor () {
+    this.mailProvider = new MailProvider();
+  }
 
   public async create(request: Request, response: Response) {
 
     const usersRepository = new UsersRepository();
     const userTokensRepository = new UserTokensRepository();
-    const mailProvider = new MailProvider();
 
     const { email } = request.body;
 
-    const sendEmailService = new SendEmailService(usersRepository, mailProvider, userTokensRepository);
+    const sendEmailService = new SendEmailService(usersRepository, this.mailProvider, userTokensRepository);
 
     await sendEmailService.execute(email);
 
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,12 +2,13 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 import AppError from '@shared/errors/AppError';
+import EtherealMailProvider from '@shared/container/providers/MailProvider/implementations/EtherealMailProvider';
 
 export default class SendForgotPasswordEmailService {
 
   constructor(
     private usersRepository: IUsersRepository,
-    private IMailProvider: IMailProvider,
+    private IMailProvider: IMailProvider = new EtherealMailProvider(),
     private userTokensRepository: IUserTokensRepository
   ) { }
 
@@ -16,8 +17,9 @@ export default class SendForgotPasswordEmailService {
 
     if (!user) throw new AppError('No users found with this email!', 401);
 
-    this.IMailProvider.sendMail(email, 'Pedido de recuperação de senha');
-    this.userTokensRepository.generate(user.id);
+    const { token } = await this.userTokensRepository.generate(user.id);
+
+    await this.IMailProvider.sendMail(email, `Pedido de recuperação de senha: ${token}`);
   }
 
 }
